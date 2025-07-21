2ea2b3f7583fd0f42babe0c0cf665df27a4095da
Lynn-0310 <nguyenthuylinh310vt@gmail.com>
2025-07-21 15:12:19 +0700

    update

---
+++ src/components/Icons/IconDropdown.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function IconDropdown() {
+  return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="8" height="5" viewBox="0 0 8 5" fill="none">
+          <path d="M7 1L4 4L1 1" stroke="#444545" strokeLinecap="round" strokeLinejoin="round" />
+      </svg>
+  )
+}
