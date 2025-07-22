5dda214734db2b53e168fc20423b7ebb115200c2
anhpt <phungtheanh2001@gmail.com>
2025-07-22 13:58:23 +0700

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,50 +1,53 @@
-'use client'
-import React, { useState } from 'react'
-import IconHamburger from '../icons/IconHamburger'
-import IconDropdown from '../icons/IconDropdown'
-import Sidebar from './SideBar'
-import IconMess from '../icons/IconMess'
-import IconApp from '../icons/IconApp'
-import IconHuman from '../icons/IconHuman'
-import IconFlag from '../icons/IconFlag'
-import AppMenu from './AppMenu'
-import UserMenu from './UserMenu'
+'use client';
+import React, { useState } from 'react';
+import IconHamburger from '../icons/IconHamburger';
+import IconDropdown from '../icons/IconDropdown';
+import IconMess from '../icons/IconMess';
+import IconApp from '../icons/IconApp';
+import IconHuman from '../icons/IconHuman';
+import IconFlag from '../icons/IconFlag';
+import Sidebar from './SideBar';
+import AppMenu from './AppMenu';
+import UserMenu from './UserMenu';
 
 export default function Header() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-    const [showMenu, setShowMenu] = useState(false)
-    const [showUserMenu, setShowUserMenu] = useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showUserMenu, setShowUserMenu] = useState(false);
 
-    return (
-        <>
-            <div className="flex justify-between items-center text-center container-header py-[27px] px-4">
-                <button onClick={() => setIsSidebarOpen(true)}>
-                    <IconHamburger />
-                </button>
+  return (
+    <>
+      <div className='container-header'>
+        <div className='flex justify-between items-center text-center py-[27px] px-4'>
+          <button onClick={() => setIsSidebarOpen(true)}>
+            <IconHamburger />
+          </button>
 
-                <div className="flex gap-[5.846px] items-center">
-                    <div className='flex gap-6'>
-                        <IconMess />
-                        <button
-                            onClick={() => setShowMenu(true)}
-                        >
-                            <IconApp />
-                        </button>
-                        <AppMenu show={showMenu} onClose={() => setShowMenu(false)} />
+          <div className='flex gap-[5.846px] items-center'>
+            <div className='flex gap-6'>
+              <IconMess />
+              <button onClick={() => setShowMenu(true)}>
+                <IconApp />
+              </button>
+              <AppMenu show={showMenu} onClose={() => setShowMenu(false)} />
 
-                        <button onClick={() => setShowUserMenu(true)}>
-                            <IconHuman />
-                        </button>
-                        <UserMenu show={showUserMenu} onClose={() => setShowUserMenu(false)} />
+              <button onClick={() => setShowUserMenu(true)}>
+                <IconHuman />
+              </button>
+              <UserMenu
+                show={showUserMenu}
+                onClose={() => setShowUserMenu(false)}
+              />
 
-                        <IconFlag />
-                    </div>
-
-                    <IconDropdown />
-                </div>
+              <IconFlag />
             </div>
 
-            <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
-        </>
-    )
+            <IconDropdown />
+          </div>
+        </div>
+      </div>
+
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+    </>
+  );
 }
