957bf30896f5248f8ff5f51eeeab8b32b6a9fa2d
anhpt <phungtheanh2001@gmail.com>
2025-08-28 16:46:25 +0700

    fix: test

--- src/components/header/HeaderSSR.tsx
+++ src/components/header/HeaderSSR.tsx
@@ -5,6 +5,11 @@ import { getListCategoriesCached } from '@/libs/ssr/categories';
 
 export default async function HeaderSSR() {
   const listCategories: ICategoriesResponse = await getListCategoriesCached();
-  // console.log('listCategories', listCategories);
-  return <Header listCategories={listCategories?.data} />;
+  return (
+    <>
+      {listCategories.data && (
+        <Header listCategories={listCategories.data || []} />
+      )}
+    </>
+  );
 }
