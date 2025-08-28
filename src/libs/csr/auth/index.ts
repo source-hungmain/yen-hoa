a9cb677e2fe203b07d1dd4637a9bcca6f95a11ef
anhpt <phungtheanh2001@gmail.com>
2025-08-28 17:12:59 +0700

    fix: change api

--- src/libs/csr/auth/index.ts
+++ src/libs/csr/auth/index.ts
@@ -2,7 +2,7 @@ import { IResponseLogin } from "@/interfaces/IResponseLogin";
 import { apiClient } from "../apiClient";
 
 export function authLogin(phone: string): Promise<IResponseLogin> {
-    return apiClient("/auth/login", {
+    return apiClient("/auth/login2", {
         method: "POST",
         data: { phone, phone_country: "VN" },
     });
