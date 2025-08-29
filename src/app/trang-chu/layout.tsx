3695d0875523d460113e7fc5182719f192ea7fa9
anhpt <phungtheanh2001@gmail.com>
2025-08-29 11:43:09 +0700

    test

--- src/app/trang-chu/layout.tsx
+++ src/app/trang-chu/layout.tsx
@@ -1,16 +1,10 @@
 import HeaderSSR from '@/components/header/HeaderSSR';
-import { ICategoriesResponse } from '@/interfaces/ICaterory';
-import { getListCategoriesCached } from '@/libs/ssr/categories';
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const listCategories: ICategoriesResponse = await getListCategoriesCached();
-  console.log(1);
-  console.log('listCategories', listCategories);
-  console.log(2);
   return (
     <>
       <HeaderSSR />
